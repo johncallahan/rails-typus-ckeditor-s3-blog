17e98928729b92292e1cd3bf55c3f4f6ce77d4c9
johncallahan <jcallahan@acm.org>
2012-12-01 06:35:25 +0000

    use s3 for attachments too

--- app/models/ckeditor/attachment_file.rb
+++ app/models/ckeditor/attachment_file.rb
@@ -1,11 +1,17 @@
 class Ckeditor::AttachmentFile < Ckeditor::Asset
   has_attached_file :data,
-                    :url => "/ckeditor_assets/attachments/:id/:filename",
-                    :path => ":rails_root/public/ckeditor_assets/attachments/:id/:filename"
+  		    :storage => :s3,
+                    :s3_credentials => "#{Rails.root}/config/amazon_s3.yml",
+                    :path => ":class/:id/:basename.:extension",
+                          :styles => {}
   
   validates_attachment_size :data, :less_than => 100.megabytes
   validates_attachment_presence :data
 	
+	def url_content
+	  url(:content)
+	end
+
 	def url_thumb
 	  @url_thumb ||= begin
 	    extname = File.extname(filename).gsub(/^\./, '')
